ecc3a017713f0b6a03cce189ecc145b9c1904425
Johannes Neugschwentner <johannes@sportskred.com>
2017-11-20 09:55:12 -0400

    removing skroll

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { ScrollProvider } from 'react-skroll';
+// import { ScrollProvider } from 'react-skroll';
 
 import registerServiceWorker from './registerServiceWorker';
 
